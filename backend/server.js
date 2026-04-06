ad93e7bcaac6467cecf47d0e749d0233380fee8c
akorganicsfoodpvtltd-creator <akorganicsfoodpvtltd@gmail.com>
2026-04-06 14:13:29 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,15 +31,11 @@ app.use(cookieParser());
 const allowedOrigins = ["http://localhost:3000"];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: [
+    'https://alkissanfoods.com',
+    'https://www.alkissanfoods.com',
+    'http://localhost:3000'
+  ]
   credentials: true, // allow cookies
 }));
 
